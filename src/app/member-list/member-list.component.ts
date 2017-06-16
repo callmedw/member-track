ae56cb14a1f5d070354e429ce068e5ebc163cda5
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-06-16 11:23:31 -0700

    member list

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  members: Member[] = [
+    new Member("Beyonce", "Beyonce Inc.",
+      "New Member Coordinator", "01/01/2017", "n/a", "available", "lotsa cool stuff here", "https://factmag-images.s3.amazonaws.com/wp-content/uploads/2013/12/beyonce-121313.jpg"),
+    new Member("Malala Yousafzai", "Malala Fund",
+      "Education and Outreach Coordinator", "01/01/2017", "n/a", "available", "lotsa cool stuff here", "http://prod-upp-image-read.ft.com/b8649f28-6139-11e3-916e-00144feabdc0"),
+  ];
 
   constructor() { }
 
---
+++ src/app/member.model.ts
@@ -0,0 +1,10 @@
+export class Member {
+  constructor (public name: string,
+    public organization: string,
+    public position: string,
+    public anniversary: string,
+    public project: string,
+    public mentorship: string,
+    public bio: string,
+    public image: string) { }
+}
